146d71e5439ebfe0546e2a10d7ce4d620d9a2fca
noir4y <210010039+noir4y@users.noreply.github.com>
2026-04-01 17:37:18 +0900

    Update nvim options

--- nvim/lua/settings/null-ls.lua
+++ nvim/lua/settings/null-ls.lua
@@ -2,29 +2,15 @@ require("mason").setup()
 
 local null_ls = require("null-ls")
 local mason_null_ls = require("mason-null-ls")
+local utils = require("null-ls.utils")
 
 mason_null_ls.setup({
   ensure_installed = { "stylua", "prettier", "phpcbf" },
   automatic_installation = false,
-
-  handlers = {
-    prettier = function(source_name, methods)
-      require("mason-null-ls").default_setup(source_name, methods, {
-        extra_args = { "--single-quote", "--jsx-single-quote" },
-        prefer_local = "node_modules/.bin",
-      })
-    end,
-
-    phpcbf = function(source_name, methods)
-      require("mason-null-ls").default_setup(source_name, methods, {
-        extra_args = { "--standard=PSR12" },
-      })
-    end,
-  },
 })
 
 null_ls.setup({
-  root_dir = require("null-ls.utils").root_pattern("composer.json", "package.json", "Makefile", ".git"),
+  root_dir = utils.root_pattern("composer.json", "package.json", "Makefile", ".git"),
   sources = {
     null_ls.builtins.formatting.stylua.with({
       extra_args = {
@@ -33,6 +19,21 @@ null_ls.setup({
         "--indent-width",
         "2",
       },
+    }),
+    null_ls.builtins.formatting.prettier.with({
+      extra_args = { "--single-quote", "--jsx-single-quote" },
+      prefer_local = "node_modules/.bin",
+    }),
+    null_ls.builtins.formatting.phpcbf.with({
+      extra_args = function(params)
+        local standard = utils.root_pattern("phpcs.xml", "phpcs.xml.dist")(params.bufname)
+
+        if standard then
+          return { "--standard=" .. standard }
+        end
+
+        return { "--standard=" .. vim.fn.expand("~/.codesniffer/phpcs.xml") }
+      end,
     }),
   },
 })
